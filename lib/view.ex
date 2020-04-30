53df483924fa37b425790a5d3d3e540de0f54ee9
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-04-30 15:31:38 -0500

    enable render_association accesible from use or import

--- lib/view.ex
+++ lib/view.ex
@@ -8,6 +8,8 @@ defmodule ExMvc.View do
     quote do
       use unquote(namespace), :view
 
+      import ExMvc.View
+
       alias unquote(model), as: Model
       alias Ecto.Association.NotLoaded
 
@@ -31,29 +33,29 @@ defmodule ExMvc.View do
         render_many(models, __MODULE__, "show.json", as: :model)
       end
 
-      defp render_association(%NotLoaded{}), do: nil
-
-      defp render_association(%{__struct__: struct} = model) do
-        struct.__schema__(:fields)
-        |> Enum.filter(&(&1 not in unquote(disallowed_fields)))
-        |> Enum.map(&{&1, Map.get(model, &1)})
-        |> Map.new()
-      end
-
-      defp render_association(%{} = model) do
-        Map.to_list(model)
-        |> Enum.filter(&render_association/1)
-        |> Map.new()
-      end
-
-      defp render_association(models) when is_list(models),
-        do: Enum.map(models, &render_association/1)
-
-      defp render_association({_name, %NotLoaded{}}), do: false
-      defp render_association({field, _}) when field in @disallowed_fields, do: false
-      defp render_association(_), do: true
-
       defoverridable render: 2
     end
   end
+
+  def render_association(%NotLoaded{}), do: nil
+
+  def render_association(%{__struct__: struct} = model) do
+    struct.__schema__(:fields)
+    |> Enum.filter(&(&1 not in unquote(disallowed_fields)))
+    |> Enum.map(&{&1, Map.get(model, &1)})
+    |> Map.new()
+  end
+
+  def render_association(%{} = model) do
+    Map.to_list(model)
+    |> Enum.filter(&render_association/1)
+    |> Map.new()
+  end
+
+  def render_association(models) when is_list(models),
+    do: Enum.map(models, &render_association/1)
+
+  def render_association({_name, %NotLoaded{}}), do: false
+  def render_association({field, _}) when field in @disallowed_fields, do: false
+  def render_association(_), do: true
 end
