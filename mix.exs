e7bde9d1512a569b085af9a50f577b8f66209fcc
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2020-04-30 18:22:37 -0500

    Version Bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExMvc.MixProject do
   def project do
     [
       app: :ex_mvc,
-      version: "0.1.4",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
